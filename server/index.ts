0db84712b8c3f96ef1b0e84566b42764c390e72f
zygimantas <zygimantasaugunas@gmail.com>
2023-10-08 19:21:26 +0300

    UPD: Try to connect to DB in prod.

--- server/index.ts
+++ server/index.ts
@@ -2,22 +2,22 @@ require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
 import app from "./app";
 import { AppDataSource } from "./data-source";
 
-// const server = AppDataSource.initialize()
-//   .then(async () => {
-//     console.log("Database connected");
-//     // const user = new User();
-//     // user.firstName = "Timber";
-//     // user.lastName = "Saw";
-//     // user.age = 25;
-//     // await AppDataSource.manager.save(user);
-//     // console.log("Saved a new user with id: " + user.id);
+const server = AppDataSource.initialize()
+  .then(async () => {
+    console.log("Database connected");
+    // const user = new User();
+    // user.firstName = "Timber";
+    // user.lastName = "Saw";
+    // user.age = 25;
+    // await AppDataSource.manager.save(user);
+    // console.log("Saved a new user with id: " + user.id);
 
-//     // console.log("Loading users from the database...");
-//     // const users = await AppDataSource.manager.find(User);
-//     // console.log("Loaded users: ", users);
+    // console.log("Loading users from the database...");
+    // const users = await AppDataSource.manager.find(User);
+    // console.log("Loaded users: ", users);
 
-//     return app();
-//   })
-//   .catch((error) => console.log(error));
+    return app();
+  })
+  .catch((error) => console.log(error));
 
 module.exports = Promise.resolve(app());
