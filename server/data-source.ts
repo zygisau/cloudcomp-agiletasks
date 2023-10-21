399c407a664e90f26df42909c748db28a2566074
zygimantas <zygimantasaugunas@gmail.com>
2023-10-21 17:52:47 +0300

    UPD: Env vars. Attempt to configure aws.

--- server/data-source.ts
+++ server/data-source.ts
@@ -3,11 +3,11 @@ import { DataSource } from "typeorm";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
-  host: process.env["AZURE_MYSQL_HOST"],
-  port: Number(process.env["AZURE_MYSQL_PORT"]),
-  database: process.env["AZURE_MYSQL_DATABASE"],
-  username: process.env["AZURE_MYSQL_USER"],
-  password: process.env["AZURE_MYSQL_PASSWORD"],
+  host: process.env["MYSQL_HOST"],
+  port: Number(process.env["MYSQL_PORT"]),
+  database: process.env["MYSQL_DATABASE"],
+  username: process.env["MYSQL_USER"],
+  password: process.env["MYSQL_PASSWORD"],
   synchronize: true,
   ssl:
     process.env["NODE_ENV"] !== "development"
